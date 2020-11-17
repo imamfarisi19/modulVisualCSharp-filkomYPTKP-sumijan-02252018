5ecf03df3ec41dce8e30929fa02c0fda29ae68af
imamfarisi19 <edwardindo1@gmail.com>
2020-11-18 06:34:58 +0800

    praktikum 1 (done)

--- modulVisualCSharp-filkomYPTKP-sumijan-Feb2018/Form1.cs
+++ modulVisualCSharp-filkomYPTKP-sumijan-Feb2018/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,34 @@ namespace modulVisualCSharp_filkomYPTKP_sumijan_Feb2018
 {
    public partial class Form1 : Form
    {
+      Image File;
       public Form1()
       {
          InitializeComponent();
+      }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         DialogResult d = saveFileDialog1.ShowDialog();
+         if (d == DialogResult.OK)
+         {
+            File.Save(saveFileDialog1.FileName, ImageFormat.Jpeg);
+         }
+        }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+
+      }
+
+      private void button1_Click(object sender, EventArgs e)
+      {
+         DialogResult d = openFileDialog1.ShowDialog();
+         if (d == DialogResult.OK)
+         {
+            File = Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.Image = File;
+         }
       }
    }
 }
