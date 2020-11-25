9e5785be1ea114d27442cbdd359eb69498be3c16
imamfarisi19 <edwardindo1@gmail.com>
2020-11-25 23:01:17 +0800

    add a load file function

--- bab2-membacaDataGambar-image2/Form1.Designer.cs
+++ bab2-membacaDataGambar-image2/Form1.Designer.cs
@@ -28,20 +28,54 @@
       /// </summary>
       private void InitializeComponent()
       {
+         this.button1 = new System.Windows.Forms.Button();
+         this.pictureBox1 = new System.Windows.Forms.PictureBox();
+         this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
          this.SuspendLayout();
+         // 
+         // button1
+         // 
+         this.button1.Location = new System.Drawing.Point(13, 13);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(75, 23);
+         this.button1.TabIndex = 0;
+         this.button1.Text = "Load";
+         this.button1.UseVisualStyleBackColor = true;
+         this.button1.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // pictureBox1
+         // 
+         this.pictureBox1.Location = new System.Drawing.Point(13, 43);
+         this.pictureBox1.Name = "pictureBox1";
+         this.pictureBox1.Size = new System.Drawing.Size(493, 289);
+         this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+         this.pictureBox1.TabIndex = 1;
+         this.pictureBox1.TabStop = false;
+         // 
+         // openFileDialog1
+         // 
+         this.openFileDialog1.FileName = "openFileDialog1";
          // 
          // Form1
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(518, 344);
+         this.Controls.Add(this.pictureBox1);
+         this.Controls.Add(this.button1);
          this.Name = "Form1";
          this.Text = "Form1";
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
          this.ResumeLayout(false);
 
       }
 
-      #endregion
-   }
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
 }
 
--- bab2-membacaDataGambar-image2/Form1.cs
+++ bab2-membacaDataGambar-image2/Form1.cs
@@ -12,9 +12,20 @@ namespace bab2_membacaDataGambar_image2
 {
    public partial class Form1 : Form
    {
+      Bitmap objBitmap;
       public Form1()
       {
          InitializeComponent();
       }
-   }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         DialogResult d = openFileDialog1.ShowDialog();
+         if (d == DialogResult.OK)
+         {
+            objBitmap = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = objBitmap;
+         }
+        }
+    }
 }
